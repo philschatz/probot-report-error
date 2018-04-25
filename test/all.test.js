6d9e5be50a367f68f64c9d300b5a5442f9c4ad3b
Philip Schatz <253202+philschatz@users.noreply.github.com>
2018-04-25 00:05:05 -0500

    :white_check_mark: :memo:  add implementation and tests

--- test/all.test.js
+++ test/all.test.js
@@ -4,35 +4,69 @@ const fetch = require('fetch-vcr')
 const Context = require('probot/lib/context')
 const Octokit = require('@octokit/rest')
 
-fetch.configure({
-  fixturePath: pathJoin(__dirname, './_fixtures/')
-})
+const reportIssue = require('../lib/report-issue')
 
-const github = new Octokit()
-const log = undefined
-const event = {
-  payload: {
-    repository: {
-      name: 'test',
-      owner: {
-        login: 'philschatz'
-      }
-    }
-  }
-}
-
-if (process.env.GITHUB_TOKEN) {
-  github.authenticate({
-    type: 'token',
-    token: process.env.GITHUB_TOKEN
+function useFixture (name) {
+  fetch.configure({
+    fixturePath: pathJoin(__dirname, '_fixtures', name)
   })
 }
-const context = new Context(event, github, log)
 
 describe('Sanity check', () => {
-  it('Can get a single repository', async () => {
-    return context.github.repos.get(context.repo()).then(resp => {
-      console.log(resp.data)
+  let context
+
+  beforeEach(() => {
+    // Create a Probot Context for each test.
+    // The payload is just enough for `context.repo()` to work.
+    // Also, use the GitHub token in case we are recording the fixtures.
+    const github = new Octokit()
+    const log = undefined
+    const event = {
+      payload: {
+        repository: {
+          name: 'test',
+          owner: {
+            login: 'philschatz'
+          }
+        }
+      }
+    }
+
+    if (process.env.GITHUB_TOKEN) {
+      github.authenticate({
+        type: 'token',
+        token: process.env.GITHUB_TOKEN
+      })
+    }
+    context = new Context(event, github, log)
+  })
+
+  it('Can get a single repository (sanity check)', async () => {
+    useFixture('sanity-check')
+    return context.github.repos.get(context.repo())
+  })
+
+  it('Can create a new Issue when one does not exist yet', async () => {
+    useFixture('fresh-issue')
+    return reportIssue(context, {
+      title: 'ReportIssue Title',
+      body: 'ReportIssue Body'
+    })
+  })
+
+  it('Does not create a new Issue when one is already open', async () => {
+    useFixture('already-open-issue')
+    return reportIssue(context, {
+      title: 'ReportIssue Title',
+      body: 'ReportIssue Body'
+    })
+  })
+
+  it('Reopens an Issue when one is closed', async () => {
+    useFixture('already-closed-issue')
+    return reportIssue(context, {
+      title: 'ReportIssue Title',
+      body: 'ReportIssue Body'
     })
   })
 })
